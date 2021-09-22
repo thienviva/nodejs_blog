ec7c7a7f6db50b001c44ac816595500461d32024
thienviva <thienviva7761311@gmail.com>
2021-09-22 16:54:59 +0700

    [ThienHN]

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ const path = require('path');
 const app = express();
 const port = 3000
 
+const route = require('./routes');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 // HTTP loger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 // Template engine
 app.engine('hbs', handlebars({
   extname: 'hbs'
@@ -17,16 +23,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// console.log('PATH: ', path.join(__dirname, 'resources/views'))
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
