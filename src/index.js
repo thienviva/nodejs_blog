0a946b58ec7d2772efaf828b210c5b2654e995c9
thienviva <thienviva7761311@gmail.com>
2021-09-23 15:17:16 +0700

    [ThienHN]

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const morgan = require('morgan')
-const handlebars  = require('express-handlebars');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 const path = require('path');
 const app = express();
-const port = 3000
+const port = 3000;
 
 const route = require('./routes');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // HTTP loger
 //app.use(morgan('combined'))
 // Template engine
-app.engine('hbs', handlebars({
-  extname: 'hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: 'hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -27,5 +31,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
